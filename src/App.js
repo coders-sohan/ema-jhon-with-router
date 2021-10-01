d3cf59b0b2fa56c8ae517432c5e5d8da09e4ec8a
coders-sohan <coders.sohan.0216@gmail.com>
2021-10-01 15:08:02 +0600

    new commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import './App.css';
-import Header from './components/Header/Header';
-import Shop from './components/Shop/Shop';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Inventory from "./components/Inventory/Inventory";
+import NotFound from "./components/NotFound/NotFound";
+import OrderReview from "./components/OrderReview/OrderReview";
+import Shop from "./components/Shop/Shop";
 
 function App() {
-  return (
-    <div>
-      <Header></Header>
-      <Shop></Shop>
-    </div>
-  );
+	return (
+		<div>
+			<Router>
+				<Header></Header>
+				<Switch>
+					<Route exact path="/">
+						<Shop />
+					</Route>
+					<Route path="/shop">
+						<Shop />
+					</Route>
+					<Route path="/review">
+						<OrderReview />
+					</Route>
+					<Route path="/inventory">
+						<Inventory />
+					</Route>
+					<Route exact path="*">
+						<NotFound />
+					</Route>
+				</Switch>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
