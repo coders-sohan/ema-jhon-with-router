bfe9eea8ffab782325725a669bacc4987c9a062e
coders-sohan <coders.sohan.0216@gmail.com>
2021-10-01 17:09:24 +0600

    new commit

--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import useProducts from '../Hooks/useProducts';
+import React from "react";
+import useProducts from "../Hooks/useProducts";
+import useCart from "../Hooks/useCart";
 
 const OrderReview = () => {
-    const [products] = useProducts();
-    return (
-        <div>
-            <h1>Products length: {products.length}</h1>
-            <h2>This is Order Review</h2>
-        </div>
-    );
+	const [products, setProducts] = useProducts();
+	const [cart] = useCart(products);
+	return (
+		<div>
+			<h1>Products length: {products.length}</h1>
+			<h1>Cart length: {cart.length}</h1>
+			<h2>This is Order Review</h2>
+		</div>
+	);
 };
 
 export default OrderReview;
