52434e87c906b87d17d381cf017eb04ddf22ace1
coders-sohan <coders.sohan.0216@gmail.com>
2021-10-01 15:35:55 +0600

    new commit

--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import useProducts from '../Hooks/useProducts';
 
 const OrderReview = () => {
+    const [products] = useProducts();
     return (
         <div>
+            <h1>Products length: {products.length}</h1>
             <h2>This is Order Review</h2>
         </div>
     );
